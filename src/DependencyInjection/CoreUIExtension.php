3cc237097bf8cdfcd1dae489579210401457a151
ahmadertarizqi <ahmadderta.rizqi30@gmail.com>
2022-09-22 15:53:17 +0700

    namespace

--- src/DependencyInjection/CoreUIExtension.php
+++ src/DependencyInjection/CoreUIExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace CoreUI\DependencyInjection;
+namespace Rizk\CoreUI\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
