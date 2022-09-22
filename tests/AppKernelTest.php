48579f1504bbe4037d6431474f734e5791807894
ahmadertarizqi <ahmadderta.rizqi30@gmail.com>
2022-09-22 15:53:30 +0700

    initial test

---
+++ tests/AppKernelTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Rizk\Coreui\CoreUIBundle\Tests;
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernelTest extends Kernel
+{
+  public function registerBundles()
+  {
+    
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader)
+  {
+    
+  }
+}
---
+++ tests/CoreUIBundleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rizk\Coreui\CoreUIBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class CoreUIBundleTest extends WebTestCase
+{
+  public static function getKernelClass() 
+  {
+    return AppKernelTest::class;
+  }
+}
